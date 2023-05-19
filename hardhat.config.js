ba5e5439c3d73731efebc0d58720caed12542b66
Dom <nicnic77@gmail.com>
2023-05-19 17:04:19 +0100

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,60 +8,6 @@ require("solidity-coverage")
 require("@nomiclabs/hardhat-truffle5")
 require("hardhat-gas-reporter")
 
-/// ENVVAR
-// - CI:                output gas report to file instead of stdout
-// - COVERAGE:          enable coverage report
-// - ENABLE_GAS_REPORT: enable gas report
-// - COMPILE_MODE:      production modes enables optimizations (default: development)
-// - COMPILE_VERSION:   compiler version (default: 0.8.9)
-// - COINMARKETCAP:     coinmarkercat api key for USD value in gas report
-
-const fs = require("fs")
-const path = require("path")
-const argv = require("yargs/yargs")()
-	.env("")
-	.options({
-		coverage: {
-			type: "boolean",
-			default: false
-		},
-		gas: {
-			alias: "enableGasReport",
-			type: "boolean",
-			default: false
-		},
-		gasReport: {
-			alias: "enableGasReportPath",
-			type: "string",
-			implies: "gas",
-			default: undefined
-		},
-		mode: {
-			alias: "compileMode",
-			type: "string",
-			choices: ["production", "development"],
-			default: "development"
-		},
-		ir: {
-			alias: "enableIR",
-			type: "boolean",
-			default: false
-		},
-		compiler: {
-			alias: "compileVersion",
-			type: "string",
-			default: "0.8.13"
-		},
-		coinmarketcap: {
-			alias: "coinmarketcapApiKey",
-			type: "string"
-		}
-	}).argv
-
-for (const f of fs.readdirSync(path.join(__dirname, "hardhat"))) {
-	require(path.join(__dirname, "hardhat", f))
-}
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -106,8 +52,6 @@ module.exports = {
 	initialBaseFeePerGas: 0,
 	gasReporter: {
 		showMethodSig: true,
-		currency: "USD",
-		outputFile: argv.gasReport,
-		coinmarketcap: argv.coinmarketcap
+		currency: "USD"
 	}
 }
